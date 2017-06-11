f0fa7f03a4ad035a287e1f4b702512267deb891f
gfrewqpoiu <Kevin.Striek@googlemail.com>
2017-06-11 22:10:16 +0200

    Block Loli and Shota

--- src/NadekoBot/Services/Searches/SearchesService.cs
+++ src/NadekoBot/Services/Searches/SearchesService.cs
@@ -123,7 +123,7 @@ namespace NadekoBot.Services.Searches
                     website = $"https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
                     break;
                 case DapiSearchType.Gelbooru:
-                    website = $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
+                    website = $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}+-loli+-shota";
                     break;
                 case DapiSearchType.Rule34:
                     website = $"https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
@@ -132,7 +132,7 @@ namespace NadekoBot.Services.Searches
                     website = $"https://konachan.com/post.xml?s=post&q=index&limit=100&tags={tag}";
                     break;
                 case DapiSearchType.Yandere:
-                    website = $"https://yande.re/post.xml?limit=100&tags={tag}";
+                    website = $"https://yande.re/post.xml?limit=100&tags={tag}+-loli+-shota";
                     break;
             }
             try
