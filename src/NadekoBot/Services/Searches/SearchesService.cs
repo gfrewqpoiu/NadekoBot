4eb0ffab3c69baca3b0be629e71d1697cb1f4d53
gfrewqpoiu <Kevin.Striek@googlemail.com>
2017-07-03 16:23:42 +0200

    Fix Search Service

--- src/NadekoBot/Services/Searches/SearchesService.cs
+++ src/NadekoBot/Services/Searches/SearchesService.cs
@@ -119,57 +119,9 @@ namespace NadekoBot.Services.Searches
 
         public Task<ImageCacherObject> DapiSearch(string tag, DapiSearchType type, ulong? guild, bool isExplicit = false)
         {
-<<<<<<< HEAD
-            tag = tag?.Replace(" ", "_");
-            var website = "";
-            switch (type)
-            {
-                case DapiSearchType.Safebooru:
-                    website = $"https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
-                    break;
-                case DapiSearchType.Gelbooru:
-                    website = $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}+-loli+-shota";
-                    break;
-                case DapiSearchType.Rule34:
-                    website = $"https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={tag}";
-                    break;
-                case DapiSearchType.Konachan:
-                    website = $"https://konachan.com/post.xml?s=post&q=index&limit=100&tags={tag}";
-                    break;
-                case DapiSearchType.Yandere:
-                    website = $"https://yande.re/post.xml?limit=100&tags={tag}+-loli+-shota";
-                    break;
-            }
-            try
-            {
-                var toReturn = await Task.Run(async () =>
-                {
-                    using (var http = new HttpClient())
-                    {
-                        http.AddFakeHeaders();
-                        var data = await http.GetStreamAsync(website).ConfigureAwait(false);
-                        var doc = new XmlDocument();
-                        doc.Load(data);
-
-                        var node = doc.LastChild.ChildNodes[new NadekoRandom().Next(0, doc.LastChild.ChildNodes.Count)];
-
-                        var url = node.Attributes["file_url"].Value;
-                        if (!url.StartsWith("http"))
-                            url = "https:" + url;
-                        return url;
-                    }
-                }).ConfigureAwait(false);
-                return toReturn;
-            }
-            catch
-            {
-                return null;
-            }
-=======
             var cacher = _imageCacher.GetOrAdd(guild, (key) => new SearchImageCacher());
             
             return cacher.GetImage(tag, isExplicit, type);
->>>>>>> Kwoth/1.4
         }
     }
     
