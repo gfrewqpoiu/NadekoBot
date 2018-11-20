d4725bfa6f644c0812fde6454323772f201d7673
Kevin <Kevin.Striek@googlemail.com>
2018-11-20 17:08:32 +0100

    remove all cheats

--- NadekoBot.Core/Modules/Gambling/Gambling.cs
+++ NadekoBot.Core/Modules/Gambling/Gambling.cs
@@ -482,10 +482,6 @@ namespace NadekoBot.Modules.Gambling
 
             var rnd = new NadekoRandom().Next(0, 101);
             Console.WriteLine(Context.User.Id);
-            if (Context.User.Id == 167311142744489984){
-                rnd = new NadekoRandom().Next(68, 101);
-                Console.WriteLine($"{Context.User.Username} has cheated with betroll");
-            }
             var str = Format.Bold(Context.User.ToString()) + Format.Code(GetText("roll", rnd));
             if (rnd < 1)
             {
